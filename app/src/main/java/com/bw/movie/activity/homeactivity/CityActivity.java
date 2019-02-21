d37431796e59f76311577806d88c2d73a0e360fa
lenovo <1375317144@qq.com>
2019-02-21 19:13:11 +0800

    Merge branch 'master' of https://github.com/WeekApp/WeekApp
    # Conflicts:
    #	app/src/main/java/com/bw/movie/fragment/cinemafragment/CinemaFragment.java

--- app/src/main/java/com/bw/movie/activity/homeactivity/CityActivity.java
+++ app/src/main/java/com/bw/movie/activity/homeactivity/CityActivity.java
@@ -1,11 +1,23 @@
 package com.bw.movie.activity.homeactivity;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +29,23 @@ import android.widget.TextView;
 import com.bw.movie.Utils.cinema.QGridView;
 import com.bw.movie.adapter.cinemaadapter.CYBChangeCityGridViewAdapter;
 import com.bw.movie.adapter.cinemaadapter.ContactAdapter;
+import com.bw.movie.bean.cinemabean.CinemaLocationBean;
 import com.bw.movie.bean.cinemabean.ToastUtil;
 import com.bw.movie.bean.cinemabean.UserEntity;
+import com.bw.movie.fragment.cinemafragment.CinemaFragment;
 import com.bw.onlymycinema.R;
+import com.google.gson.Gson;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -42,6 +67,11 @@ public class CityActivity extends AppCompatActivity {
     private ArrayList<String> list;
     private ImageView pic_contact_back;
     private Intent intent;
+    private String locationProvider;
+    private double mLatitude;
+    private double mLongitude;
+    private String mCity1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,7 +80,11 @@ public class CityActivity extends AppCompatActivity {
         initview();
         initAdapter();
         onlisten();
+        //获取经纬度
+        initlongitude();
 
+        //获取请求到的定位
+        initLocation();
     }
 
     public void initAdapter(){
@@ -155,8 +189,11 @@ public class CityActivity extends AppCompatActivity {
                     finish();
                 }
             });
-
+            vh.item_header_city_dw.setText(mCity1);
         }
+
+
+
 
         private class VH extends RecyclerView.ViewHolder {
             GridView head_home_change_city_gridview;
@@ -179,5 +216,141 @@ public class CityActivity extends AppCompatActivity {
             list.add(contactEntity);
         }
         return list;
+    }
+
+
+    //获取经纬度
+    private void initlongitude() {
+        LocationManager lm = (LocationManager)CityActivity.this. getSystemService(Context.LOCATION_SERVICE);
+
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);//低精度，如果设置为高精度，依然获取不了location。
+        criteria.setAltitudeRequired(false);//不要求海拔
+        criteria.setBearingRequired(false);//不要求方位
+        criteria.setCostAllowed(true);//允许有花费
+        criteria.setPowerRequirement(Criteria.POWER_LOW);//低功耗
+
+        locationProvider = lm.getBestProvider(criteria, true);
+
+        if (ActivityCompat.checkSelfPermission(CityActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(CityActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("asd", "onCreate: 没有权限 ");
+            return;
+        }
+
+        Location location = lm.getLastKnownLocation(locationProvider);
+
+        if (location != null) {
+            Log.d("a", "onCreate: location");
+            //不为空,显示地理位置经纬度
+            showLocation(location);
+        }
+
+        //监视地理位置变化
+        lm.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+
+
+        /*Location location = LocationUtils.getInstance( getContext() ).showLocation();
+        if (location != null) {
+            String address = "纬度：" + location.getLatitude() + "经度：" + location.getLongitude();
+            mLatitude = location.getLatitude();
+            mLongitude = location.getLongitude();
+            System.out.println("精度: "+mLatitude+"");
+            System.out.println("精度: "+mLongitude+"");
+            Log.d( "FLY.LocationUtils", address );
+            Toast.makeText(getContext(), mLatitude+" =qaz= "+mLongitude, Toast.LENGTH_SHORT).show();
+        }*/
+    }
+    LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle arg2) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d("a", "onProviderEnabled: " + provider + ".." + Thread.currentThread().getName());
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d("a", "onProviderDisabled: " + provider + ".." + Thread.currentThread().getName());
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("a", "onLocationChanged: " + ".." + Thread.currentThread().getName());
+            //如果位置发生变化,重新显示
+            showLocation(location);
+        }
+
+
+    };
+
+    private void showLocation(Location location) {
+//获取维度信息
+        mLatitude = location.getLatitude();
+        //获取经度信息
+        mLongitude = location.getLongitude();
+
+        Log.i("获取经纬度",  "  维度：" + mLatitude + "  经度：" + mLongitude);
+
+    }
+    //获取GPS定位的城市
+    private void initLocation() {
+
+        final MyAsyncExtue myAsyncExtue = new MyAsyncExtue();
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                String s = myAsyncExtue.doInBackground();
+
+                myAsyncExtue.onPostExecute(s);
+            }
+        }.start();
+    }
+
+
+    //获取当前定位的城市
+    private class MyAsyncExtue extends AsyncTask<Location, Void, String> {
+        @Override
+        protected String doInBackground(Location... params) {
+
+
+            HttpClient client = new DefaultHttpClient();
+            StringBuilder stringBuilder = new StringBuilder();
+            HttpGet httpGet = new HttpGet("http://api.map.baidu.com/geocoder?output=json&location="+ mLatitude +","+ mLongitude +"&ak=I3Bm5iocjMlbwGayEm1B3VXkWBmV9t76");
+            try {
+                HttpResponse response = client.execute(httpGet);
+                HttpEntity entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String b;
+                while ((b = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(b + "\n");
+                }
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return stringBuilder.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String m_list) {
+            super.onPostExecute(m_list);
+            Log.e("str", m_list.toString());
+            String city = "";
+//                if (m_list != null && m_list.size() > 0) {
+//                    city = m_list.get(0).getLocality();//获取城市
+//                }
+            city = m_list;
+            CinemaLocationBean cinemaLocationBean = new Gson().fromJson(city, CinemaLocationBean.class);
+            mCity1 = cinemaLocationBean.getResult().getAddressComponent().getCity();
+
+
+        }
     }
 }
